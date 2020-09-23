d40ef245cd5ca92ea2713bf7d1fd8de356af80dc
lixiny <siriusnever@163.com>
2020-09-23 17:04:26 +0800

    add checkpoints existence check based on ISSUE #1.

--- bihand/models/net_bihand.py
+++ bihand/models/net_bihand.py
@@ -14,14 +14,14 @@ from bihand.utils.misc import param_count
 
 class NetBiHand(nn.Module):
     def __init__(
-            self,
-            net_modules=['seed', 'lift', 'sik'],  # ['seed','lift'],['seed','lift','sik']
-            njoints=21,
-            inp_res=256,
-            out_hm_res=64,
-            out_dep_res=64,
-            upstream_hg_stacks=2,
-            upstream_hg_blocks=1,
+        self,
+        net_modules=["seed", "lift", "sik"],  # ['seed','lift'],['seed','lift','sik']
+        njoints=21,
+        inp_res=256,
+        out_hm_res=64,
+        out_dep_res=64,
+        upstream_hg_stacks=2,
+        upstream_hg_blocks=1,
     ):
 
         super(NetBiHand, self).__init__()
@@ -38,55 +38,40 @@ class NetBiHand(nn.Module):
             out_dep_res=out_dep_res,
         )
 
-        cprint('params upstream: {:.3f}M'.format(
-            param_count(self.upstream)), 'green', attrs=['bold'])
-        if 'sik' in net_modules:
+        cprint("params upstream: {:.3f}M".format(param_count(self.upstream)), "green", attrs=["bold"])
+        if "sik" in net_modules:
             self.siknet = SIKNet()
-            cprint('params siknet: {:.3f}M'.format(
-                param_count(self.siknet)), 'blue', attrs=['bold'])
+            cprint("params siknet: {:.3f}M".format(param_count(self.siknet)), "blue", attrs=["bold"])
 
     def load_checkpoints(
-            self,
-            ckp_seednet=None,
-            ckp_liftnet=None,
-            ckp_siknet=None,
+        self, ckp_seednet=None, ckp_liftnet=None, ckp_siknet=None,
     ):
-        if (
-                ckp_seednet
-                and os.path.isfile(ckp_seednet)
-                and 'seed' in self.net_modules
-        ):
-            misc.load_checkpoint(
-                self.upstream.seednet, ckp_seednet
-            )
+        # NOTE: File exist check! (refer to issue #1)
+        if not os.path.isfile(ckp_seednet):
+            cprint(f"{ckp_seednet} is not exist", "red")
+        if not os.path.isfile(ckp_liftnet):
+            cprint(f"{ckp_liftnet} is not exist", "red")
+        if not os.path.isfile(ckp_siknet):
+            cprint(f"{ckp_siknet} is not exist", "red")
 
-        if (
-                ckp_liftnet
-                and os.path.isfile(ckp_liftnet)
-                and 'lift' in self.net_modules
-        ):
-            misc.load_checkpoint(
-                self.upstream.liftnet, ckp_liftnet
-            )
+        if ckp_seednet and os.path.isfile(ckp_seednet) and "seed" in self.net_modules:
+            misc.load_checkpoint(self.upstream.seednet, ckp_seednet)
 
-        if (
-                ckp_siknet
-                and os.path.isfile(ckp_siknet)
-                and 'sik' in self.net_modules
-        ):
-            misc.load_checkpoint(
-                self.siknet, ckp_siknet
-            )
+        if ckp_liftnet and os.path.isfile(ckp_liftnet) and "lift" in self.net_modules:
+            misc.load_checkpoint(self.upstream.liftnet, ckp_liftnet)
+
+        if ckp_siknet and os.path.isfile(ckp_siknet) and "sik" in self.net_modules:
+            misc.load_checkpoint(self.siknet, ckp_siknet)
 
     def forward(self, img, infos):
 
-        ''' ***** net upstream ***** '''
+        """ ***** net upstream ***** """
         ups_result, ups_enc = self.upstream(img, infos)
 
-        ''' ***** net mano ***** '''
+        """ ***** net mano ***** """
         sik_reslut = {}
-        if 'sik' in self.net_modules:
-            joint = ups_result['l_joint'][-1]  # B, 21, 3
+        if "sik" in self.net_modules:
+            joint = ups_result["l_joint"][-1]  # B, 21, 3
             jointRS, kin_chain = self.siknet.parse_input(joint, infos)
             sik_reslut = self.siknet(jointRS, kin_chain)
 
